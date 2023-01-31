d0fc701f96ca05b9dde3d85760e0ea0c192b7298
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2023-01-31 13:06:05 +0100

    Saplings grow into trees

--- src/tile/placeables/sapling.js
+++ src/tile/placeables/sapling.js
@@ -1,25 +1,19 @@
 import { sprites } from "../../game/graphics/loadAssets.js";
-import PlaceableBase from "../base/placeableBase.js";
+import { BasicTree } from "../../structure/structureParent.js";
+import SaplingBase from "../base/saplingBase.js";
 
-export class Sapling extends PlaceableBase {
+export class Sapling extends SaplingBase {
     constructor(gridX,gridY,world) {
         super(gridX,gridY,world);
         this.setRegistryName("placeable_sapling");
         this.setSprite(sprites.placeables.sapling);
-        this.toolType = "axe";
-        this.miningLevel = 0;
-        this.miningTime = 0.2;
-        this.requireTool = true;
-        
+
         this.tileDrops = [
             {id:8,rate:50,amount:1,requireTool:false}
         ];
-    }
 
-    tileUpdate() {
-        if(!this.world.getTile(this.gridX, this.gridY - 1)) {
-            this.breakTile();
-        }
+        this.tree = new BasicTree(this.gridX,this.gridY,this.world);
+        this.growthValue = 2000;
     }
 
     draw() {
