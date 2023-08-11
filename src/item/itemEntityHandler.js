caffc6098a2efb41f4e9f035bd85e56995375fa7
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2023-08-11 13:07:25 +0200

    addEntity method returns the new entity

--- src/item/itemEntityHandler.js
+++ src/item/itemEntityHandler.js
@@ -7,15 +7,17 @@ export default class ItemEntityHandler {
         this.entities = [];
     }
 
-    addEntity(x, y, dx, dy, stackSize, item) {
-        this.entities.push(new ItemEntity(x, y, dx, dy, stackSize, item, this.game));
+    addEntity(x, y, stackSize, item) {
+        let entity = new ItemEntity(x, y, stackSize, item, this.game);
+        this.entities.push(entity);
+        return entity;
     }
 
     update(m) {
         for(let i = 0; i < this.entities.length; i++) {
             this.entities[i].update(m);
     
-            if(overlap(this.entities[i],this.game.player)) {
+            if(overlap(this.entities[i], this.game.player)) {
                 let removed = this.entities[i].pickUp(this.game.player);
                 if(removed) {
                     this.entities.splice(i,1);
