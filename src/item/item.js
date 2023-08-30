da7367a1e71a620d254f94844512c4516dc7744c
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2023-08-30 13:15:15 +0200

    item rarities enum moved to own file

--- src/item/item.js
+++ src/item/item.js
@@ -1,28 +1,6 @@
 import { getDescription, getDisplayName, getLang } from "../game/lang.js";
 import { sprites } from "../game/graphics/loadAssets.js";
-
-const ITEM_RARITIES = {
-    "COMMON": 0,
-    "UNCOMMON": 1,
-    "RARE": 2,
-    "EPIC": 3,
-    "LEGENDARY": 4,
-    "UNOBTAINABLE": 99,
-}
-
-const RARITY_COLORS = {};
-
-const addRarityColor = (rarityName, rgb) => {
-    let r = ITEM_RARITIES[rarityName];
-    if(r) RARITY_COLORS[r] = rgb;
-}
-
-addRarityColor("COMMON", {r:240, g:240, b:240});
-addRarityColor("UNCOMMON", {r:100, g:200, b:120});
-addRarityColor("RARE", {r:80, g:150, b:220});
-addRarityColor("EPIC", {r:170, g:110, b:255});
-addRarityColor("LEGENDARY", {r:255, g:180, b:0});
-addRarityColor("UNOBTAINABLE", {r:220, g:0, b:30});
+import { ITEM_RARITIES, RARITY_COLORS } from "./rarities.js";
 
 export default class Item {
     constructor(game, registryName) {
